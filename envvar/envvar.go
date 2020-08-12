bf04f45f61d74f52d13c4d789c0965f7e1164a6f
tangx <uyinn@live.com>
2020-08-12 13:35:44 +0800

    feat: init

---
+++ envvar/envvar.go
@@ -0,0 +1,118 @@
+package envvar
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+// GetEnv get enviroment varible's value into struct
+func GetEnv(ptr interface{}, prefix string) error {
+
+	m := make(map[string]interface{})
+
+	// rv for reflect Value
+	rvPtr := reflect.ValueOf(ptr)
+	if rvPtr.Kind() != reflect.Ptr {
+		msg := fmt.Sprintf("Want a Struct Prt, Got a %s", rvPtr.Kind())
+		return errors.New(msg)
+	}
+
+	// sValue := reflect.Indirect(reflect.ValueOf(v))
+	rv := reflect.Indirect(rvPtr)
+	if rv.Kind() != reflect.Struct {
+		msg := fmt.Sprintf("Want a Struct, Got a %s", rv.Kind())
+		return errors.New(msg)
+	}
+
+	typ := rv.Type()
+	for i := 0; i < typ.NumField(); i++ {
+		sFiled := typ.Field(i)
+		// spew.Dump(sFiled)
+
+		tag, ok := sFiled.Tag.Lookup("env")
+		if !ok {
+			continue
+		}
+
+		name := strings.Split(tag, ",")[0]
+		if name == "-" {
+			continue
+		}
+
+		envName := fmt.Sprintf("%s_%s", prefix, strings.ToUpper(name))
+		envValue := os.Getenv(envName)
+
+		switch sFiled.Type.Kind() {
+		case reflect.String:
+			m[name] = envValue
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			m[name] = MustParseInteger(envValue)
+		case reflect.Bool:
+			m[name] = MustParseBool(envValue)
+		}
+	}
+	// spew.Dump(m)
+
+	b, err := json.Marshal(m)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(b, ptr)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// SetEnv set struct value to envirotment
+func SetEnv(v interface{}, prefix string) {
+
+	rv := reflect.Indirect(reflect.ValueOf(v))
+
+	typ := rv.Type()
+	for i := 0; i < typ.NumField(); i++ {
+		sFiled := typ.Field(i)
+		if tag, ok := sFiled.Tag.Lookup("env"); ok {
+			th := strings.Split(tag, ",")[0]
+			if th == "-" {
+				continue
+			}
+
+			sValue := rv.Field(i)
+			envName := fmt.Sprintf("%s_%s", prefix, strings.ToUpper(th))
+
+			switch sValue.Kind() {
+			case reflect.String:
+				os.Setenv(envName, sValue.String())
+			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32:
+				// os.Setenv(envName, string(sValue.Int()))
+				os.Setenv(envName, strconv.FormatInt(sValue.Int(), 10))
+			case reflect.Bool:
+				os.Setenv(envName, strconv.FormatBool(sValue.Bool()))
+			}
+		}
+	}
+}
+
+func MustParseInteger(s string) (n int64) {
+	n, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	return
+}
+
+func MustParseBool(s string) (ok bool) {
+	ok, err := strconv.ParseBool(s)
+	if err != nil {
+		panic(err)
+	}
+	return
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/tangx/go-envvars
+
+go 1.14
+
+require (
+	github.com/coredns/coredns v1.7.0
+	github.com/smartystreets/goconvey v0.0.0-20190330032615-68dc04aab96a
+)
